945d5e81a8c257d66d2f8fc7ac0cbf769c051fa8
Md-Mosabbir <kmosabbir@gmail.com>
2022-12-26 16:13:33 +0600

    01 02 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    
+    if ( num < 0){
+        return "ERROR"
+    }
 
+    let strings = '';
+    for ( let i = 0; i< num; i++){
+        strings += word;
+    }
+    
+    return strings;
+
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
