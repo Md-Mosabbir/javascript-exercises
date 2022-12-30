4b01be16e4951c4e17f503d4e629f95a6a2d7cca
Md-Mosabbir <kmosabbir@gmail.com>
2022-12-30 15:01:25 +0600

    Finished :>

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let Celsius = (f -32) * 5/9;
+
+  return Math.round(Celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let Fahrenheit = (c * 9/5 +32);
+  return Math.round(Fahrenheit * 10) / 10
+
 };
 
 // Do not edit below this line
