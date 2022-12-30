b807e17d0ddcc71a0323cd8c0cf4b545f2875196
Md-Mosabbir <kmosabbir@gmail.com>
2022-12-30 18:30:26 +0600

    Calculator X

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(c, d) {
+  return c -d;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.length
+  ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+  : 0;
 
 };
 
-const power = function() {
+const power = function(base, power) {
+  return base ** power;
 	
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 	
 };
 
